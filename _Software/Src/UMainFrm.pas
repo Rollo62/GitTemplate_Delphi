d1fe2a627dbecc18e77927a2c514cf6e0324f99f
Rollo <plokijuh9>
2015-12-02 22:50:54 +0100

    After Magnifier solved

--- _Software/Src/UMainFrm.pas
+++ _Software/Src/UMainFrm.pas
@@ -4,12 +4,14 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.TabControl,
-  FMX.Controls.Presentation, FMX.MultiView, FMX.StdCtrls, FMX.ListBox,
-  FMX.Layouts, FMX.ScrollBox, FMX.Memo, FMX.ListView.Types,
-  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, FMX.ListView,
-  FMX.Objects, FMX.Edit, FMX.ExtCtrls
+  FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
+  FMX.ListBox, FMX.Layouts, FMX.MultiView, FMX.ScrollBox, FMX.Memo,
+  FMX.ExtCtrls, FMX.Objects, FMX.Edit, FMX.StdCtrls, FMX.Controls.Presentation,
+  FMX.ListView, FMX.Controls, FMX.TabControl,
+  FMX.Types, FMX.Forms, FMX.Graphics, FMX.Dialogs
   , FMX.Ani
+  , System.Diagnostics
+  , S4.Math.Statistics
   , S4.ImageViewer
   , S4.ImageViewer.Controller
   ;
@@ -51,25 +53,50 @@ type
     TextPhotoResult: TText;
     ImageViewer1: TImageViewer;
     TextPhotoHitData: TText;
+    Memo1: TMemo;
+    TabItem5: TTabItem;
+    Button4: TButton;
+    Button5: TButton;
+    ListBoxItem04_TestLog: TListBoxItem;
+    Button6: TButton;
+    TrackBar1: TTrackBar;
+    Image1: TImage;
+    Button7: TButton;
     procedure ListBox1ItemClick(const Sender: TCustomListBox;
       const Item: TListBoxItem);
 
+    procedure Setup_Default;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
 
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+//##    procedure TrackBar1Change(Sender: TObject);
+//##    procedure Button7Click(Sender: TObject);
+    procedure FormResize(Sender: TObject);
 
 
   private
     { Private declarations }
     FTabFkt: TProc;
 
+
     //
     // ImageViewer1 stuff
     //
     FImageViewController : TS4ImageViewController;
     IImageViewer: IS4ImageViewer;
+    FTimerTestLog: TTimer;
+    FStopWatch: TStopwatch;
+    FAverage: TS4Math_Average;
+    FSelX: Single;
+    FSelY: Single;
+
+    procedure EvTestLogTimer(Sender: TObject);
 
 
     procedure TabControl_ChangeTo(const Item: TListBoxItem);
@@ -101,6 +128,9 @@ Uses
   , FMX.MediaLibrary
   , S4.Types
   , S4.Media.Photo
+  , S4.Media.Gallery
+  , S4.Log
+  , S4.Control.Screenshot
   ;
 
 const
@@ -176,13 +206,385 @@ begin
 
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.Button6Click(Sender: TObject);
 begin
+
+  TS4Media_TakeGallery.Execute(TSize.Create(EditPhotoSize.Text.ToInteger,
+                                          EditPhotoSize.Text.ToInteger),
+                             False //Do not store in Album
+                             // Image ready
+                             , procedure (const bmpPhoto : TBitmap)
+
+  var
+    sP: Single;
+  begin
+
+                               IImageViewer.Bitmap.Assign( bmpPhoto );
+
+//                               ImageViewer1.Bitmap.Assign(bmpPhoto);
+//
+//                               TextPhotoResult.Text := 'Result W: ' +          bmpPhoto.Width.ToString  +
+//                                                       ', H: '      +          bmpPhoto.Height.ToString +
+//                                                       Format(' Scale %1.4f', [bmpPhoto.BitmapScale])
+//                                                       ;
+//
+//                               Bitmap_Changed(bmpPhoto.Width, bmpPhoto.Height);
+
+                               TabControl_Refresh_TakePhoto;
+
+                               sP := 1.0;
+                               FImageViewController.ScalePicture := sP;
+
+                             end
+                             // Image cancelled
+                             , procedure
+                             begin
+                               IImageViewer.Bitmap_Clear;// := nil;
+
+                               ShowMessage('Take Photo was cancelled');
+                             end
+                            );
+
+
+end;
+
+////##
+//procedure TForm1.Button7Click(Sender: TObject);
+//var
+//  ptSel: TPointF;
+//  ptAbs: TPointF;
+//  iv: TImageViewer;
+//  I: Integer;
+//  FImgLocal: TImage;
+//  ptLoc: TPointF;
+//  rcImg: TRectF;
+//  iW: Integer;
+//  iH: Integer;
+//  FMagLen: Integer;
+//  ptFac: TPointF;
+////  rcSrc: TRectF;
+//  sWindowX: Single;
+//  sWindowY: Single;
+//  ptBmp: TPointF;
+//  rcDst: TRectF;
+//  bmpBck: TBitmap;
+//  sL: Single;
+//  sR: Single;
+//  sT: Single;
+//  sB: Single;
+//  rcBck: TRectF;
+//  rcSrc: TRectF;
+//  bmpSnap: TBitmap;
+//
+//begin
+//  ptSel := PointF(FSelX, FSelY);
+//  ptAbs := ImageViewer1.LocalToAbsolute( ptSel );
+//
+//  //
+//  // Seek the internal Image for direct access
+//  //
+//  iv        := ImageViewer1;
+//  FImgLocal := nil;
+//
+//  for I := 0 to iv.ComponentCount-1 do
+//  begin
+//    if iv.Components[I] is TImage then
+//    begin
+//      FImgLocal := (iv.Components[I] as TImage);
+//      break;
+//    end;
+//  end;
+//
+//
+//  Image1.Bitmap.BitmapScale := FImgLocal.Bitmap.BitmapScale;
+////  Image1.Bitmap.SetSize(100, 100);
+//
+//  // Check is Image1 has already right Aspect ratio
+//
+//  rcDst := Image1.LocalRect;
+//
+//  rcSrc := RectF(0,0,
+//                 FImgLocal.Bitmap.Width, FImgLocal.Bitmap.Height
+//                );
+//  rcDst := rcSrc.FitInto( rcDst );
+//  rcDst.SetLocation(0, 0);  // Ensure 0,0 Position for the bitmap copy
+//
+//  Image1.Bitmap.SetSize( Trunc(rcDst.Width), Trunc(rcDst.Height) );
+//
+//
+//
+//  ptLoc := FImgLocal.AbsoluteToLocal( ptAbs );
+//
+//  rcImg := RectF( FImgLocal.Position.X,
+//                  FImgLocal.Position.Y,
+//                 (FImgLocal.Position.X + FImgLocal.Width ),
+//                 (FImgLocal.Position.Y + FImgLocal.Height)
+//                );
+//
+//  iW := Trunc(iv.ContentBounds.Width);
+//  iH := Trunc(iv.ContentBounds.Height);
+//
+//  FMagLen := Trunc(FImgLocal.Width / 4);
+//
+////  ptLoc.X := ptLoc.X - rcImg.Left;
+////  ptLoc.Y := ptLoc.Y - rcImg.Top;
+//
+//
+//  // Factor on Bitmap
+//  ptFac := PointF( S4Types_Limit(0.0,  (ptLoc.X ) / rcImg.Width,  1.0),
+//                   S4Types_Limit(0.0,  (ptLoc.Y ) / rcImg.Height, 1.0)
+//                 );
+//
+//
+//  iW := iv.Bitmap.Width;
+//  iH := iv.Bitmap.Height;
+//
+//
+////  // Get Desired Bitmap size
+////  rcSrc := RectF(0,0, iW div 2, iH div 2);  // Can I use smaller bmp ?
+//
+//
+//  Image1.Bitmap.Canvas.BeginScene;
+//  Image1.Bitmap.Clear( TAlphaColorRec.Yellow );
+//
+//  // Show Frame markers
+//
+//  Image1.Bitmap.Canvas.StrokeThickness := 1.5;
+//  Image1.Bitmap.Canvas.Stroke.Color    := TAlphaColorRec.Blue;
+//
+//  rcDst := RectF(0,0,2,2);
+//
+//  //TL
+//  rcDst.SetLocation(0,0);
+//  Image1.Bitmap.Canvas.DrawRect( rcDst, 0, 0, [],  1);
+//  //TR
+//  rcDst.SetLocation( Image1.Bitmap.Width - rcDst.Width,0);
+//  Image1.Bitmap.Canvas.DrawRect( rcDst, 0, 0, [],  1);
+//  //BL
+//  rcDst.SetLocation(0, Image1.Bitmap.Height - rcDst.Height);
+//  Image1.Bitmap.Canvas.DrawRect( rcDst, 0, 0, [],  1);
+//  //BR
+//  rcDst.SetLocation( Image1.Bitmap.Width - rcDst.Width, Image1.Bitmap.Height - rcDst.Height);
+//  Image1.Bitmap.Canvas.DrawRect( rcDst, 0, 0, [],  1);
+//
+//
+//
+//  try
+//
+//    begin
+//
+//      if iW <= iH then
+//      begin
+//        sWindowX := iW / 8;   // FMagLen; // 100; // * GIS4SysInfo.ScreenScale / 2;
+//        sWindowY := sWindowX; // sWindowX * iH / iW; // CorrectBy Aspect
+//      end
+//      else
+//      begin
+//        sWindowY := iH / 8;   // FMagLen; // 100; // * GIS4SysInfo.ScreenScale / 2;
+//        sWindowX := sWindowY; //sWindowY * iW / iH; // CorrectBy Aspect
+//      end;
+//
+//      if FImageViewController.ScalePicture > 1.0 then
+//      begin
+//        sWindowX := sWindowX / FImageViewController.ScalePicture; // Only zoomed Images rescale
+//        sWindowY := sWindowY / FImageViewController.ScalePicture; // Only zoomed Images rescale
+//      end;
+//
+//      //
+//      // Center the View Window
+//      //
+//
+//      //######ssZ := 1/2;// / sScreen;
+//
+//      ptBmp := PointF(ptFac.X * iW,  ptFac.Y * iH );
+//
+////      // Acc. to Scale factor of Screenshot
+////      rcSrc := RectF(ptBmp.X - sWindowX,
+////                     ptBmp.Y - sWindowY,
+////                     ptBmp.X + sWindowX*1,
+////                     ptBmp.Y + sWindowY*1
+////                    );
+//
+//
+////      // Check is Image1 has already right Aspect ratio
+////
+////
+////      rcDst := RectF(0,
+////                     0,
+////                     Image1.Bitmap.Width,
+////                     Image1.Bitmap.Height
+////                    );
+//
+//
+//      //1st draw Background Bitmap
+//      if iv is TImageViewer then
+//        bmpBck := iv.Bitmap
+//      else
+//        bmpBck := nil;
+//      if Assigned(bmpBck) then
+//      begin
+//
+//        sL := ptBmp.X - sWindowX;
+//        if sL < 0 then
+//        begin
+//          sL := 0;
+//          sR := sL + sWindowX * 2.0;
+//        end
+//        else
+//        begin
+//
+//          sR := ptBmp.X + sWindowX;
+//          if sR >= iW then
+//          begin
+//            sR := iW - 1;
+//            sL := sR - sWindowX*2
+//          end;
+//
+//        end;
+//
+//        sT := ptBmp.Y - sWindowY;
+//        if sT < 0 then
+//        begin
+//          sT := 0;
+//          sB := sT + sWindowY * 2;
+//        end
+//        else
+//        begin
+//
+//          sB := ptBmp.Y + sWindowY;
+//          if sB >= iH then
+//          begin
+//            sB := iH - 1;
+//            sT := sB - sWindowY*2
+//          end;
+//
+//        end;
+//
+//
+////OOOFF        rcBck := RectF( sL, sT, sR, sB );
+//
+//        // Background Bitmap to Magnifier
+////        Image1.Bitmap.Canvas.DrawBitmap( bmpBck,
+////                                         rcBck,
+////                                         rcDst,
+////                                         1
+////                                       );
+//
+//
+//        //SanpShot the whole ImageDrawer,
+//        rcSrc   := RectF(0, 0, bmpBck.Width, bmpBck.Height);
+//        bmpSnap := S4Ctrl_MakeScreenshot4( FImageViewController.Drawer.Drawer, rcSrc.Size );
+//
+//
+//
+//        rcDst := RectF(0,
+//                       0,
+//                       Image1.Bitmap.Width,
+//                       Image1.Bitmap.Height
+//                      );
+//
+//
+//
+//        // Drawing to Magnifier
+//        if Assigned(bmpSnap) then
+//          Image1.Bitmap.Canvas.DrawBitmap( bmpSnap,
+//                                           //rcBck,
+//                                           rcSrc,  //RectF(0,0,bmp.Width, bmp.Width),
+//                                           rcDst,
+//                                           1
+//                                          );
+//
+//
+//
+//      end;
+//
+//    end;
+//
+//  finally
+//
+//      Image1.Bitmap.Canvas.EndScene;
+//
+//      bmpBck := nil;
+//  end;
+//
+//
+//
+//
+//end;
+
+
+
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  Self.Tag              := 0;
+  FTimerTestLog.Enabled := True;
+
+  FStopWatch.Reset;
+  FAverage.  Reset;
+
+  TabControlMain.ActiveTab := TabItem4;
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  FTimerTestLog.Enabled := False;
+end;
+
+
+procedure TForm1.EvTestLogTimer(Sender: TObject);
+begin
+  FTimerTestLog.Enabled := False;
+
+  Self.Tag := Self.Tag + 1;
+
+  FStopWatch.Reset; // !! Must Reset, otherwise takes lap count
+  FStopWatch.Start;
+
+  GIS4Log.Add( Format('Timer: %7d, Avg: %2.6f', [Self.Tag, FAverage.Mean] ) );
+
+  FStopWatch.Stop;
+
+  FAverage.Add( FStopWatch.ElapsedMilliseconds );
+
+  FTimerTestLog.Enabled := True;
+end;
+
+
+procedure TForm1.Setup_Default;
+begin
+
+//  FStopWatch := TStopWatch.Create;
+//  FAverage   := TS4Math_Average.Create;
+////              procedure TForm1.Setup_Default;
+//
+//
+//  FTimerTestLog           := TTimer.Create(Self);
+//  FTimerTestLog.Enabled   := False;
+//  FTimerTestLog.Interval  := 10;
+//  FTimerTestLog.OnTimer   := EvTestLogTimer;
+
+  Memo1.Lines.Insert(0, 'Initial');
+
+  GIS4Log.Subscribe_LogProc( procedure (const aMsg : String)
+                             begin
+                               //Memo1.Lines.Strings[0] := aMsg;  // Do not Add
+                               Memo1.Lines.Insert(0, aMsg);
+                             end
+                            );
+
+
   TabControlMain.TabPosition := TTabPosition.None;
   TabControlMain.TabIndex    := 0;
   FTabFkt                    := nil;
 
-  FImageViewController := TS4ImageViewController.Create( ImageViewer1 );
+
+
+  //
+  // ImageViewer Part
+  //
+
+  FImageViewController := TS4ImageViewController.Create( ImageViewer1, TrackBar1 );
   IImageViewer         := FImageViewController; // From now on this is my access interface
   IImageViewer.SetScaleLimits(IImageViewer.ScaleMin, // TS4ImageViewController. ScaleMin,
                               100);
@@ -198,6 +600,11 @@ begin
 
   IImageViewer.OnSelectionChanged :=  procedure (Arg1, Arg2, Arg3 : Single)
                                   begin
+                                    FSelX := Arg1;
+                                    FSelY := Arg2;
+
+//Off, as this was TestView only with Image1                                    Button7Click(nil);
+
                                     if  Arg3 > 0.0 then
                  TextPhotoHitData.Text := Format('Sel.X: %4.0f, Y: %4.0f, DRAW', [Arg1, Arg2])
   else
@@ -208,11 +615,51 @@ begin
 end;
 
 
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+  FStopWatch := TStopWatch.Create;
+  FAverage   := TS4Math_Average.Create;
+//              procedure TForm1.Setup_Default;
+
+
+  FTimerTestLog           := TTimer.Create(Self);
+  FTimerTestLog.Enabled   := False;
+  FTimerTestLog.Interval  := 10;
+  FTimerTestLog.OnTimer   := EvTestLogTimer;
+
+  Image1.Bitmap.SetSize(100, 100);
+  Image1.Bitmap.Clear( TAlphaColorRec.Yellow );
+
+end;
+
+
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
-  IImageViewer         := nil;
+
+  FTimerTestLog.Enabled := False;
+  FTimerTestLog.OnTimer := nil;
+
+  IImageViewer          := nil;
   FImageViewController.Free;
-  FImageViewController := nil;
+  FImageViewController  := nil;
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  // Do I recognize the Orientation Change ?
+  // Android: Yes
+  if Assigned(FImageViewController) then
+  begin
+    FImageViewController.Magnifier.Setup_Size; // Resize
+  end;
+
+
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Setup_Default;
 end;
 
 procedure TForm1.TabControl_ChangeTo(const Item: TListBoxItem);
@@ -233,6 +680,9 @@ begin
   begin
      FTabFkt := TabControl_Refresh_TakePhoto;
   end;
+
+  // Do the Refresh onece after Change
+  TabControl_Refresh;
 
 end;
 
@@ -669,12 +1119,18 @@ end;
 
 procedure TForm1.TabControl_Refresh_TakePhoto;
 begin
+  IImageViewer.Bitmap_Changed( IImageViewer.Bitmap.Width, IImageViewer.Bitmap.Height);
   IImageViewer.Arrange_BestFit;
 end;
 
 
 
 
+////##
+//procedure TForm1.TrackBar1Change(Sender: TObject);
+//begin
+//  FImageViewController.ScalePicture := TrackBar1.Value
+//end;
 
 procedure TForm1.ListBox1ItemClick(const Sender: TCustomListBox; const Item: TListBoxItem);
 begin
